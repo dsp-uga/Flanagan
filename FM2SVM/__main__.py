9a3193b8942701ec2144ef78073ebca38064ed12
Vinay Kumar <vinay.bingi@gmail.com>
2018-04-05 13:24:54 -0400

    Update __main__.py
    corrected a typo: line 13 "training data samples" to "test data samples"

--- FM2SVM/__main__.py
+++ FM2SVM/__main__.py
@@ -4,13 +4,13 @@ import FM2SVM
 
 def main():
 	parser = argparse.ArgumentParser(description='CSCI 8360 - Project 4, Team Flanagan, module FM2SVM\
-														implements flatten metrics + SVM')
+							implements flatten metrics + SVM')
 
 	parser.add_argument("-d", "--trainset", default = "data/train/smalldata/",
 						help = "Path to folder containing a training data samples")
 
 	parser.add_argument("-t", "--testset", default = "data/test/smalldata/",
-						help = "Path to folder containing a training data samples")
+						help = "Path to folder containing a test data samples")
 
 	parser.add_argument("-o", "--output", default = "output",
 						help = "Path to directory where output will be written")
